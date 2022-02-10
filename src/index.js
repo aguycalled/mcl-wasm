61f5eea7877ca05ddc4873087b2bff1827052a2e
alex <alex@nav.community>
2022-02-10 16:34:50 +0100

    Release 0.8.2

--- src/index.js
+++ src/index.js
@@ -1,5 +1,5 @@
 const crypto = require('crypto')
-const mclCreateModule = require('./mcl_c.js')
+const mclCreateModule = typeof WebAssembly === "object" ? require('./mcl_c.js') : require('./mcl_c_nowasm.js')
 const mclSetupFactory = require('./mcl')
 
 const getRandomValues = crypto.randomFillSync
